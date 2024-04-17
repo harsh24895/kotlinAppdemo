c693aa7679f034eaea84dc85f2e2634cd6ada13e
Harsh <harshpatel24895@gmail.com>
2024-04-16 21:13:07 -0300

    New

---
+++ app/src/main/java/com/example/kotlintestpts/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.kotlintestpts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.buttonOnclickme
+import kotlinx.android.synthetic.main.activity_main.editTextName
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val btn:Button = findViewById(R.id.buttonOnclickme)
+//        btn.setOnClickListener {  }
+
+        buttonOnclickme.setOnClickListener {
+            val name = editTextName.text
+
+            val message = "Hello $name";
+
+           Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+
+        }
+
+
+
+
+        }
+
+
+
+    }
